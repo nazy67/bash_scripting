e2bc19e02af31c9b67485215497ce4a5799a8733
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-04-06 21:42:37 -0500

    Update var_on_cli.sh

---
+++ var_on_cli.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# passing variables to scripts at the command line
+clear
+
+# variables will be passed at the command line
+MYFIRSTNAME=$1
+MYLASTNAME=$2
+MYPASSWORD=$3
+
+echo "My name is $MYFIRSTNAME and my lastname is $MYLASTNAME. My password $MYPASSWORD"
