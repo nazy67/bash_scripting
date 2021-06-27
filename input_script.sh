298b9cb1f4bdee3a364c19618ef23ef8a0b088d4
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-06-27 09:20:54 -0500

    added input_script.sh

---
+++ input_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Author: Eliza
+# Date
+# Desc:
+a=`hostname`
+echo Hello, my server name $a
+echo
+echo What is your name?
+read b
+echo
+echo What is your profession?
+read c
+echo
+echo What is your favorite show?
+read d
+echo
+echo Hello $b
+echo Your profession $c is awesome
+echo $d is a good show
+echo
