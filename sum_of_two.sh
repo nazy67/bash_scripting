2aee469c3f37bde1b649d5a8ec4b3744b95a8e3f
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-06-27 09:25:00 -0500

    added sum_of_two.sh

---
+++ sum_of_two.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# The script mulitplies two numbers
+read -p "Enter first number: " number1
+read -p "Enter second number: " number2
+ 
+muliply=$(( $number1 * $number2 ))
+ 
+echo "The result is: $multiply"
